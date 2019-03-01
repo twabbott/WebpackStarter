636d52254a5f8f773f55d555b35e2e1229a85d2f
Tom Abbott <tabbott@dealersocket.com>
2019-03-01 12:08:12 -0700

    Commit all the things

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles/styles.css';
+import mustang from './images/Mustang-GT.png';
+
+const element = document.getElementById('app');
+element.innerText = 'Hello, world!';
+
+const img = document.getElementById('mustang');
+img.src = mustang;
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js"]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devServer: {
+    contentBase: "./dist"
+  }
+};
