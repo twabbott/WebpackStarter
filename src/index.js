21993ed63d07f073e2dc229e2c39cf367fcd1558
Tom Abbott <tabbott@dealersocket.com>
2019-03-04 11:12:52 -0700

    Better placeholder content

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import './styles/styles.css';
 import mustang from './images/Mustang-GT.png';
 
-const element = document.getElementById('app');
-element.innerText = 'Hello, world!';
+// Get rid of all the old content so we can put in new stuff.
+const app = document.getElementById('app');
+while (app.firstChild) {
+    app.firstChild.remove();
+}
 
-const img = document.getElementById('mustang');
-img.src = mustang;
+// Add congratulations header
+let newElement = document.createElement('h1');
+newElement.innerText = 'Congratulations!';
+app.appendChild(newElement);
+
+// Add two paragraphs explaining what to do next.
+newElement = document.createElement('p');
+newElement.innerText = 'You did it!  Now you are ready to start bringing your own files into this project.';
+app.appendChild(newElement);
+
+newElement = document.createElement('p');
+newElement.innerText = 'You\'ll need to stop webpack-dev-server before you go to the next step.  Go to your command terminal and press Ctrl+C.';
+app.appendChild(newElement);
+
+// Cool car!
+newElement = document.createElement('img');
+newElement.src = mustang;
+newElement.classList.add('centered-image');
+app.appendChild(newElement);
